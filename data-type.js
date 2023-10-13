f5809fd7f91352cbaa34907e6d9f3d6e5847b808
Rahul_islam <103566678+Rahulislam1177@users.noreply.github.com>
2023-10-13 16:09:30 +0600

    Add files via upload

---
+++ data-type.js
@@ -0,0 +1,11 @@
+let a =20;
+let b =a;
+a =10;
+//console.log(a,b);
+
+
+const x ={job:'React Debolepar'};
+const y = x;
+
+y.job = 'Full strack Deboleper';
+console.log(x,y);
---
+++ equal.js
@@ -0,0 +1,8 @@
+const first =3;
+const seced = '3';
+if(first === seced){
+    console.log('condetion is true');
+}
+else{
+    console.log('condition is fals');
+}
